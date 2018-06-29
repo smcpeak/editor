smcpeak/editor

commit e51760f1c3f8668c9d3ebb98da1f01d53b511ccd

Fix leak of TDEF object in EditorWidget.  Require non-null editor.
EW::forgetAboutFile needed to 'deleteIt' when it forgets about a TDF.

I cleaned up some comments and unnecessary flexibility in EW::editor;
now it is always non-null and specified at the start.  Added some
related assertions in EW.

Added leak detection for TDE class (using object count at end).

main.cc
@@ -62,6 +62,9 @@ EditorWindow::EditorWindow(GlobalState *theState, TextDocumentFile *initFile,
     fileChoiceActions(),
     isearch(NULL)
 {
+  xassert(theState);
+  xassert(initFile);
+
   // will build a layout tree to manage sizes of child widgets
   QVBoxLayout *mainArea = new QVBoxLayout();
   mainArea->setObjectName("mainArea");
@@ -85,7 +88,7 @@ EditorWindow::EditorWindow(GlobalState *theState, TextDocumentFile *initFile,
   editorFrame->setFrameStyle(QFrame::Box);
   editArea->addWidget(editorFrame, 0 /*row*/, 0 /*col*/);
 
-  this->editorWidget = new EditorWidget(NULL /*temporary*/, this->statusArea);
+  this->editorWidget = new EditorWidget(initFile, this->statusArea);
   this->editorWidget->setObjectName("editor widget");
   editorFrame->addWidget(this->editorWidget);
   this->editorWidget->setFocus();
@@ -110,7 +113,7 @@ EditorWindow::EditorWindow(GlobalState *theState, TextDocumentFile *initFile,
                     800,800);     // initial size
 
   // Set the TextDocumentFile, which was originally set as NULL above.
-  this->setDocumentFile(initFile);
+  //this->setDocumentFile(initFile);
 
   // i-search; use filename area as the status display.
   this->isearch = new IncSearch(this->statusArea);
@@ -871,7 +874,7 @@ GlobalState::~GlobalState()
 EditorWindow *GlobalState::createNewWindow(TextDocumentFile *initFile)
 {
   EditorWindow *ed = new EditorWindow(this, initFile);
-  ed->setObjectName("main editor window");
+  ed->setObjectName("Editor Window");
   rebuildWindowMenus();
 
   // NOTE: caller still has to say 'ed->show()'!
@@ -950,12 +953,35 @@ void GlobalState::deleteDocumentFile(TextDocumentFile *file)
 }
 
 
-static void printObjectCounts(char const *when)
+// Possibly print counts of allocated objects.  Return their sum.
+static int printObjectCountsIf(char const *when, bool print)
 {
-  cout << "Counts " << when << ':' << endl;
-  PVAL(EditorWidget::objectCount);
-  PVAL(EditorWindow::objectCount);
-  PVAL(TextDocumentFile::objectCount);
+  if (print) {
+    cout << "Counts " << when << ':' << endl;
+  }
+
+  // Current count of outstanding objects.
+  int sum = 0;
+
+  #define PRINT_COUNT(var) \
+    sum += (var);          \
+    if (print) {           \
+      PVAL(var);           \
+    }
+
+  PRINT_COUNT(EditorWidget::objectCount);
+  PRINT_COUNT(EditorWindow::objectCount);
+  PRINT_COUNT(TextDocumentFile::objectCount);
+  PRINT_COUNT(TextDocumentEditor::s_objectCount);
+
+  #undef PRINT_COUNT
+
+  return sum;
+}
+
+static int maybePrintObjectCounts(char const *when)
+{
+  return printObjectCountsIf(when, tracingSys("objectCount"));
 }
 
 
@@ -977,13 +1003,17 @@ int main(int argc, char **argv)
       return 4;
     }
 
-    if (tracingSys("objectCount")) {
-      printObjectCounts("before GlobalState destruction");
-    }
+    maybePrintObjectCounts("before GlobalState destruction");
   }
 
-  if (tracingSys("objectCount")) {
-    printObjectCounts("after GlobalState destruction");
+  int remaining = maybePrintObjectCounts("after GlobalState destruction");
+  if (remaining != 0) {
+    // Force the counts to be printed so we know more about the problem.
+    printObjectCountsIf("after GlobalState destruction", true);
+
+    cout << "WARNING: Allocated objects at end is " << remaining
+         << ", not zero!\n"
+         << "There is a leak or use-after-free somewhere." << endl;
   }
 
   return ret;

.gdbinit
@@ -10,14 +10,12 @@ file ./editor
 #file ./buffer
 #file ./td
 
-set args -tr objectCount main.h main.cc editor-widget.h editor-widget.cc
+set args -tr objectCount td.h
 
 #set args tmp
 
 break breaker
 break abort
 break main
-
-#break editor.cc:1554
 
 run

editor-widget.cc
@@ -63,6 +63,7 @@ EditorWidget::EditorWidget(TextDocumentFile *tdf, StatusDisplay *status_,
     infoBox(NULL),
     status(status_),
     editor(new TextDocumentFileEditor(tdf)),
+    m_editors(),
     topMargin(1),
     leftMargin(1),
     interLineSpace(0),
@@ -81,7 +82,10 @@ EditorWidget::EditorWidget(TextDocumentFile *tdf, StatusDisplay *status_,
     nonfocusCursor(),
     ignoreScrollSignals(false)
 {
+  xassert(tdf);
   xassert(status_);
+
+  m_editors.prepend(editor);
 
   setFonts(bdfFontData_editor14r,
            bdfFontData_editor14i,
@@ -276,7 +280,7 @@ void EditorWidget::forgetAboutFile(TextDocumentFile *file)
   // Remove 'file' from my list.
   for(ObjListMutator< TextDocumentFileEditor > mut(m_editors); !mut.isDone(); ) {
     if (mut.data()->file == file) {
-      mut.remove();
+      mut.deleteIt();
     }
     else {
       mut.adv();

editor-widget.h
@@ -68,11 +68,8 @@ private:     // data
   StatusDisplay *status;         // (serf)
 
   // ------ editing state -----
-  // Editor object for the file we are editing; it can be set to NULL when
-  // the ctor is called, but must if so, must be set to a non-NULL
-  // value via setDocumentFile() before any drawing or editing is done.
-  //
-  // When non-NULL, this points at one of the elements of 'm_editors'.
+  // Editor object for the file we are editing.  Never NULL.
+  // This points at one of the elements of 'm_editors'.
   TextDocumentFileEditor *editor;
 
   // All of the editors associated with this widget.  An editor is

td-editor.cc
@@ -4,6 +4,9 @@
 #include "td-editor.h"                 // this module
 
 
+int TextDocumentEditor::s_objectCount = 0;
+
+
 TextDocumentEditor::TextDocumentEditor(TextDocument *doc)
   : m_doc(doc),
     m_cursor(),
@@ -18,12 +21,17 @@ TextDocumentEditor::TextDocumentEditor(TextDocument *doc)
     // their own size though.
     m_lastVisible(4, 9)
 {
+  xassert(doc);
   selfCheck();
+
+  TextDocumentEditor::s_objectCount++;
 }
 
 
 TextDocumentEditor::~TextDocumentEditor()
-{}
+{
+  TextDocumentEditor::s_objectCount--;
+}
 
 
 void TextDocumentEditor::selfCheck() const

td-editor.h
@@ -26,6 +26,10 @@ class TextDocumentEditor {
   // want to ensure I do not do it accidentally.
   NO_OBJECT_COPIES(TextDocumentEditor);
 
+public:      // static data
+  // Constructions minus destructions, for bug detection.
+  static int s_objectCount;
+
 private:     // data
   // The document we are editing.  This is the true "document" in the
   // sense of representing an abstract mathematical object of interest

commit e1a41da7e2e53536301051bec80b5d6c7958cb9d

Fix bug: Window title is wrong on second and later windows.
This is a bug I introduced in e51760f1 when I commented-out the call to
setDocumentFile in the EW constructor.  I meant to come back to that but
forgot.

main.cc
@@ -115,8 +115,8 @@ EditorWindow::EditorWindow(GlobalState *theState, TextDocumentFile *initFile,
   this->setGeometry(400,100,      // initial location
                     800,800);     // initial size
 
-  // Set the TextDocumentFile, which was originally set as NULL above.
-  //this->setDocumentFile(initFile);
+  // Set scrollbar ranges, status bar text, and window title.
+  this->updateForChangedFile();
 
   // i-search; use filename area as the status display.
   this->isearch = new IncSearch(this->statusArea);