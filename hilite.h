9a64424f24161871145d9f30c85f0e40c80241e6
Scott McPeak <scott.g.mcpeak@gmail.com>
2018-07-01 13:02:58 -0700

    hilite.h: Add comments clarifying the interface, including why it is an observer.

--- hilite.h
+++ hilite.h
@@ -9,14 +9,24 @@
 class LineCategories;                  // textcategory.h
 
 
+// A highlighter can apply TextCategories to lines of text.  The
+// renderer converts those categories to specific colors, etc.
+//
+// A highlighter is an observer so it can maintain its incremental
+// highlighting state.
 class Highlighter : public TextDocumentObserver {
 public:
-  // clients *are* allowed to delete objects known only as
-  // implementors of Highlighter
+  // Clients *are* allowed to delete objects known only as
+  // implementors of Highlighter.
   virtual ~Highlighter() {}
 
-  // Fill 'categories' with the styles for 'line' in 'buf'.
-  virtual void highlight(TextDocumentCore const &buf, int line,
+  // Fill 'categories' with the styles for 'line' in 'doc'.
+  //
+  // Although 'doc' is a parameter here, a highlighter object is always
+  // associated with a specific document object (via a mechanism that
+  // depends on the particular implementor class), and 'highlight' must
+  // only be passed a reference to that particular object.
+  virtual void highlight(TextDocumentCore const &doc, int line,
                          LineCategories &categories)=0;
 };
 
