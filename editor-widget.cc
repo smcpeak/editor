smcpeak/editor

commit 0c9a4bda14b439eb2069bbf14e095e230286ee4b

Add initial version of "Launch command" feature.
Add ProcessWatcher class.

Make GlobalState observe the document list.

EditorWidget: Always listen to the document.  That is necessary so we
can see the updates happening from the child process, and it resolves
a long-standing design bug.

TextInputDialog: Add window title to constructor.  Internally detect
already-open condition.

TextDocument: Add m_isProcessOutput.

editor-widget.cc
@@ -116,6 +116,7 @@ EditorWidget::EditorWidget(FileTextDocument *tdf,
   // empty, but it also adds it to m_editorList and may initialize the
   // view from another window.
   m_editor = this->getOrMakeEditor(tdf);
+  this->startListening();
 
   m_documentList->addObserver(this);
 
@@ -169,6 +170,9 @@ void EditorWidget::selfCheck() const
 
   // Check that 'm_listening' agrees with the document's observer list.
   xassert(m_listening == m_editor->hasObserver(this));
+
+  // And, at this point, we should always be listening.
+  xassert(m_listening);
 }
 
 
@@ -262,10 +266,7 @@ void EditorWidget::setFonts(char const *normal, char const *italic, char const *
 
 void EditorWidget::setDocumentFile(FileTextDocument *file)
 {
-  bool wasListening = m_listening;
-  if (wasListening) {
-    this->stopListening();
-  }
+  this->stopListening();
 
   m_editor = this->getOrMakeEditor(file);
 
@@ -273,10 +274,7 @@ void EditorWidget::setDocumentFile(FileTextDocument *file)
   // now the most recently used.
   m_documentList->moveFile(file, 0);
 
-  if (wasListening) {
-    this->startListening();
-  }
-
+  this->startListening();
   this->checkForDiskChanges();
   this->redraw();
 }
@@ -1827,13 +1825,6 @@ void EditorWidget::focusInEvent(QFocusEvent *e)
   TRACE("focus", "editor(" << (void*)this << "): focus in");
   QWidget::focusInEvent(e);
 
-  // I don't want to listen while I'm adding changes of
-  // my own, because the way the view moves (etc.) on
-  // changes is different.
-  //
-  // TODO: This should be removed.  It is not reliable.
-  this->stopListening();
-
   this->checkForDiskChanges();
 }
 
@@ -1842,12 +1833,6 @@ void EditorWidget::focusOutEvent(QFocusEvent *e)
 {
   TRACE("focus", "editor(" << (void*)this << "): focus out");
   QWidget::focusOutEvent(e);
-
-  stopListening();    // just in case
-
-  // listen to my docFile for any changes coming from
-  // other windows
-  startListening();
 }
 
 
@@ -1884,6 +1869,14 @@ void EditorWidget::observeInsertLine(TextDocumentCore const &buf, int line) noex
   }
   TRACE("observe", "observeInsertLine line=" << line);
   INITIATING_DOCUMENT_CHANGE();
+
+  if (m_editor->isProcessOutput()) {
+    // Just track the end of the document.
+    m_editor->setCursor(m_editor->endCoord());
+    m_editor->scrollToCursor();
+    this->redraw();
+    return;
+  }
 
   // Internally inside HE_text::insert(), the routine that actually
   // inserts text, inserting "line N" works by removing the text on

editor-window.cc
@@ -195,6 +195,9 @@ void EditorWindow::buildMenu()
     file->addAction("&Reload", this, SLOT(fileReload()));
     file->addAction("Reload a&ll", this, SLOT(fileReloadAll()));
     file->addSeparator();
+    file->addAction("&Launch (run) command ...", this, &fileLaunchCommand,
+                    Qt::ALT + Qt::Key_R);
+    file->addSeparator();
     file->addAction("E&xit", this, SLOT(fileExit()));
   }
 
@@ -644,6 +647,23 @@ void EditorWindow::fileReloadAll()
 }
 
 
+void EditorWindow::fileLaunchCommand()
+{
+  static TextInputDialog *dialog
+    = new TextInputDialog("Launch Command");
+
+  string dir = m_editorWidget->getDocumentDirectory();
+  if (!dialog->runPrompt_nonEmpty(qstringb(
+        "Command to launch in " << dir << ":"))) {
+    return;
+  }
+
+  FileTextDocument *fileDoc =
+    m_globalState->runLaunchCommandDialog(toQString(dir), dialog->m_text);
+  this->setDocumentFile(fileDoc);
+}
+
+
 bool EditorWindow::canQuitApplication()
 {
   stringBuilder msg;
@@ -780,8 +800,7 @@ void EditorWindow::editGotoLine()
   // can share the input history.
   static TextInputDialog *dialog;
   if (!dialog) {
-    dialog = new TextInputDialog();
-    dialog->setWindowTitle("Goto Line");
+    dialog = new TextInputDialog("Goto Line");
     dialog->setLabelText("Line number:");
   }
 
@@ -792,7 +811,7 @@ void EditorWindow::editGotoLine()
   dialog->m_text = qstringb(m_editorWidget->cursorLine() + 1);
 
   if (dialog->exec()) {
-    string s = dialog->m_text.toUtf8().constData();
+    string s(toString(dialog->m_text));
 
     if (!s.isempty()) {
       int n = atoi(s);
@@ -832,28 +851,14 @@ void EditorWindow::editApplyCommand()
   // Only the variables declared above can be used after the
   // child exits, and even then only with care.
   {
-    static TextInputDialog *dialog;
-    if (!dialog) {
-      dialog = new TextInputDialog();
-      dialog->setWindowTitle("Apply Command");
-    }
-
-    if (dialog->isVisible()) {
-      // This should be impossible because it is application-modal,
-      // but I will be defensive.
-      QMessageBox::information(this, "Dialog Already Shown",
-        "The apply-command dialog is already visible elsewhere.  "
-        "There can only be one instance of that dialog open.");
-      return;
-    }
+    static TextInputDialog *dialog =
+      new TextInputDialog("Apply Command");
 
     string dir = m_editorWidget->getDocumentDirectory();
-    dialog->setLabelText(qstringb("Command to run in " << dir << ":"));
-
-    if (!dialog->exec() || dialog->m_text.isEmpty()) {
+    if (!dialog->runPrompt_nonEmpty(qstringb(
+          "Command to run in " << dir << ":"))) {
       return;
     }
-    dialog->rememberInput(dialog->m_text);
     commandString = toString(dialog->m_text);
 
     tde = m_editorWidget->getDocumentEditor();

Makefile
@@ -320,6 +320,8 @@ EDITOR_OBJS += my-table-widget.o
 EDITOR_OBJS += open-files-dialog.o
 EDITOR_OBJS += open-files-dialog.moc.o
 EDITOR_OBJS += pixmaps.o
+EDITOR_OBJS += process-watcher.o
+EDITOR_OBJS += process-watcher.moc.o
 EDITOR_OBJS += sar-panel.o
 EDITOR_OBJS += sar-panel.moc.o
 EDITOR_OBJS += status.o

command-runner.h
@@ -89,9 +89,6 @@ protected:   // funcs
   // Called when the timer expires.
   virtual void timerEvent(QTimerEvent *event) OVERRIDE;
 
-  // Kill the process if we haven't done so already.
-  void killProcess();
-
   // Send some data to the process on its input channel.
   void sendData();
 
@@ -185,6 +182,9 @@ public:      // funcs
 
   // Get the stderr data.
   QByteArray takeErrorData();
+
+  // Kill the process if we haven't done so already.
+  void killProcess();
 
   // ---------------------- process status -------------------------
   // Return true if the process has started and not terminated.  Only

doc/classes.ded
@@ -14,14 +14,14 @@
       }
     },
     {
-      "attributes": "m_core: TextDocumentCore\nm_history: HE_group\nm_historyIndex: int\nm_savedHistoryIndex: int\nm_groupStack: HE_group[]\n",
+      "attributes": "m_core: TextDocumentCore\nm_history: HE_group\nm_historyIndex: int\nm_savedHistoryIndex: int\nm_groupStack: HE_group[]\nm_isProcessOutput: bool\n",
       "loc": {
         "x": 650,
-        "y": 150
+        "y": 140
       },
       "name": "TextDocument",
       "size": {
-        "h": 110,
+        "h": 120,
         "w": 250
       }
     },
@@ -62,14 +62,14 @@
       }
     },
     {
-      "attributes": "m_documentList: FileTextDocumentList\nm_windows: EditorWindow[]\nm_pixmaps: Pixmaps\n",
+      "attributes": "m_documentList: FileTextDocumentList\nm_windows: EditorWindow[]\nm_processes: ProcessWatcher[]\nm_pixmaps: Pixmaps\n",
       "loc": {
         "x": 30,
         "y": 505
       },
       "name": "GlobalState : QApplication",
       "size": {
-        "h": 85,
+        "h": 95,
         "w": 240
       }
     },
@@ -135,11 +135,23 @@
     {
       "loc": {
         "x": 340,
-        "y": 370
+        "y": 365
       },
       "name": "FileTextDocumentListObserver",
       "size": {
-        "h": 35,
+        "h": 20,
+        "w": 250
+      }
+    },
+    {
+      "attributes": "m_fileDoc: FileTextDocument\nm_commandRunner: CommandRunner\n",
+      "loc": {
+        "x": 365,
+        "y": 535
+      },
+      "name": "ProcessWatcher",
+      "size": {
+        "h": 65,
         "w": 250
       }
     }
@@ -173,16 +185,8 @@
       "open": false,
       "parentRef": 11,
       "pt": {
-        "x": 275,
+        "x": 255,
         "y": 375
-      }
-    },
-    {
-      "open": false,
-      "parentRef": 11,
-      "pt": {
-        "x": 275,
-        "y": 400
       }
     }
   ],
@@ -299,7 +303,12 @@
       "start": {"entityRef": 4}
     },
     {
-      "end": {"inheritanceRef": 4},
+      "controlPts": [{
+        "x": 235,
+        "y": 375
+      }],
+      "end": {"inheritanceRef": 3},
+      "routingAlg": "RA_MANHATTAN_VERT",
       "start": {"entityRef": 4}
     },
     {
@@ -337,19 +346,41 @@
           "y": 435
         },
         {
-          "x": 475,
+          "x": 545,
           "y": 485
         }
       ],
       "end": {"entityRef": 10},
       "label": "m_documentList",
       "start": {"entityRef": 3}
+    },
+    {
+      "controlPts": [{
+        "x": 490,
+        "y": 475
+      }],
+      "end": {"entityRef": 2},
+      "label": "m_fileDoc",
+      "routingAlg": "RA_DIRECT",
+      "start": {"entityRef": 12}
+    },
+    {
+      "end": {
+        "arrowStyle": "AS_DOUBLE_ANGLE",
+        "entityRef": 12
+      },
+      "label": "m_processes[]",
+      "start": {"entityRef": 5}
+    },
+    {
+      "end": {"inheritanceRef": 3},
+      "start": {"entityRef": 5}
     }
   ],
   "type": "Diagram Editor Diagram",
   "version": 23,
   "windowSize": {
-    "h": 617,
+    "h": 626,
     "w": 925
   }
 }

editor-window.h
@@ -138,7 +138,7 @@ public:      // funcs
   virtual void fileTextDocumentListOrderChanged(
     FileTextDocumentList *documentList) NOEXCEPT OVERRIDE;
 
-public slots:
+public Q_SLOTS:
   void fileNewFile();
   void fileOpen();
   void fileSave();
@@ -146,6 +146,7 @@ public slots:
   void fileClose();
   void fileReload();
   void fileReloadAll();
+  void fileLaunchCommand();
   void fileExit();
 
   void editISearch();

main.cc
@@ -5,6 +5,8 @@
 
 // this dir
 #include "editor-widget.h"             // EditorWidget
+#include "process-watcher.h"           // ProcessWatcher
+#include "textinput.h"                 // TextInputDialog
 
 // smqtutil
 #include "qtutil.h"                    // toQString
@@ -16,6 +18,7 @@
 #include "trace.h"                     // TRACE
 
 #include <QMenuBar>
+#include <QMessageBox>
 #include <QStyleFactory>
 
 
@@ -41,8 +44,12 @@ GlobalState::GlobalState(int argc, char **argv)
   : QApplication(argc, argv),
     m_pixmaps(),
     m_documentList(),
-    m_windows()
+    m_windows(),
+    m_processes(),
+    m_openFilesDialog(NULL)
 {
+  m_documentList.addObserver(this);
+
   // Optionally print the list of styles Qt supports.
   if (tracingSys("style")) {
     QStringList keys = QStyleFactory::keys();
@@ -103,7 +110,9 @@ GlobalState::GlobalState(int argc, char **argv)
 
 
 GlobalState::~GlobalState()
-{}
+{
+  m_documentList.removeObserver(this);
+}
 
 
 EditorWindow *GlobalState::createNewWindow(FileTextDocument *initFile)
@@ -180,6 +189,78 @@ FileTextDocument *GlobalState::runOpenFilesDialog()
     m_openFilesDialog = new OpenFilesDialog(&m_documentList);
   }
   return m_openFilesDialog->runDialog();
+}
+
+
+FileTextDocument *GlobalState::runLaunchCommandDialog(
+  QString dir, QString command)
+{
+  // Make a new document to hold the result.
+  FileTextDocument *fileDoc = this->createNewFile();
+
+  // Make the watcher that will populate that file.
+  ProcessWatcher *watcher = new ProcessWatcher(fileDoc);
+  m_processes.prepend(watcher);
+  QObject::connect(watcher, &ProcessWatcher::signal_processTerminated,
+                   this,           &GlobalState::on_processTerminated);
+
+  // Launch the child process.
+  watcher->m_commandRunner.setProgram("sh");
+  watcher->m_commandRunner.setArguments(QStringList() << "-c" << command);
+  watcher->m_commandRunner.setWorkingDirectory(dir);
+  watcher->m_commandRunner.startAsynchronous();
+
+  TRACE("process", "dir: " << toString(dir));
+  TRACE("process", "cmd: " << toString(command));
+  TRACE("process", "started: " << watcher);
+  TRACE("process", "fileDoc: " << fileDoc);
+
+  return fileDoc;
+}
+
+
+ProcessWatcher *GlobalState::findWatcherForDoc(FileTextDocument *fileDoc)
+{
+  FOREACH_OBJLIST_NC(ProcessWatcher, m_processes, iter) {
+    ProcessWatcher *watcher = iter.data();
+    if (watcher->m_fileDoc == fileDoc) {
+      return watcher;
+    }
+  }
+  return NULL;
+}
+
+
+void GlobalState::fileTextDocumentRemoved(
+  FileTextDocumentList *documentList,
+  FileTextDocument *fileDoc) NOEXCEPT
+{
+  ProcessWatcher *watcher = this->findWatcherForDoc(fileDoc);
+  if (watcher) {
+    // Closing an output document.  Break the connection to the
+    // document so it can go away safely, and start killing the
+    // process.
+    TRACE("process", "killing: " << watcher);
+    watcher->m_fileDoc = NULL;
+    watcher->m_commandRunner.killProcess();
+  }
+}
+
+
+void GlobalState::on_processTerminated(ProcessWatcher *watcher)
+{
+  TRACE("process", "terminated: " << watcher);
+
+  // Get rid of this watcher.
+  if (!m_processes.removeIfPresent(watcher)) {
+    DEV_WARNING("ProcessWatcher terminated but not in m_processes.");
+
+    // I'm not sure where this rogue watcher came from.  We're now
+    // in recovery mode, so refrain from deallocating it.
+  }
+  else {
+    delete watcher;
+  }
 }
 
 

commit 64e5c75d02401c01f322cc0127ca2330cb361728

Fix bug: Alt+A (apply command) moved cursor due to notifying itself.
This bug was introduced in 0c9a4bda14, when I made EditorWidget
unconditionally listen to its own document.  I neglected to change
the insertText for Alt+A to suppress response to change notification.

editor-widget.cc
@@ -1836,6 +1836,15 @@ void EditorWidget::blockIndent(int amt)
 }
 
 
+void EditorWidget::insertText(char const *text, int length)
+{
+  INITIATING_DOCUMENT_CHANGE();
+  UndoHistoryGrouper ugh(*m_editor);
+  m_editor->insertText(text, length);
+  this->redraw();
+}
+
+
 // ----------------- nonfocus situation ------------------
 void EditorWidget::focusInEvent(QFocusEvent *e)
 {

editor-widget.h
@@ -299,9 +299,12 @@ public:      // funcs
   // signal that will finish the job from the window side.
   void doCloseSARPanel();
 
-  // -------------------- reformatting whitespace ---------------------
+  // ---------------------- document changes ----------------------
   // Indent or unindent selected lines.
   void blockIndent(int amt);
+
+  // Insert text at cursor, overwriting selection if active.
+  void insertText(char const *text, int length);
 
   // -------------------- interaction with files ------------------
   // nonfocus listening

editor-window.cc
@@ -917,11 +917,8 @@ void EditorWindow::editApplyCommand()
   }
 
   // Replace the selected text with the command's output.
-  {
-    UndoHistoryGrouper ugh(*tde);
-    tde->insertText(runner.getOutputData().constData(), runner.getOutputData().size());
-  }
-  m_editorWidget->redraw();
+  m_editorWidget->insertText(runner.getOutputData().constData(),
+                             runner.getOutputData().size());
 
   // For error output or non-zero exit code, we show a warning, but
   // still insert the text.  Note that we do this *after* inserting