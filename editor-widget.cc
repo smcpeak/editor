517a0e98c15343c8d7623e9930ac99ce2cdd227d
Scott McPeak <scott.g.mcpeak@gmail.com>
2018-07-14 09:13:46 -0700

    In process output, keep scroll as high as possible.
    This particularly helps after reusing a document.

--- editor-widget.cc
+++ editor-widget.cc
@@ -1920,6 +1920,7 @@ void EditorWidget::observeInsertLine(TextDocumentCore const &buf, int line) noex
     // TODO: I want a "soft" tracking here, where I track until the user
     // moves the cursor away, and then resume tracking if the user moves
     // the cursor back to the end.
+    m_editor->setFirstVisible(TextCoord(0,0));  // Scroll from top.
     m_editor->setCursor(m_editor->endCoord());
     m_editor->clearMark();
     m_editor->scrollToCursor();
