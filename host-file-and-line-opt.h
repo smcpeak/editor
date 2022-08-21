d7fe2e96cf1f55ddb78f84b057c9fc8b87e0449b
Scott McPeak <scott.g.mcpeak@gmail.com>
2022-08-21 15:13:23 -0700

    Move HostFileAndLineOpt into host-file-and-line-opt.h.

--- host-file-and-line-opt.h
+++ host-file-and-line-opt.h
@@ -4,7 +4,50 @@
 #ifndef EDITOR_HOST_FILE_AND_LINE_OPT_H
 #define EDITOR_HOST_FILE_AND_LINE_OPT_H
 
-// For the moment, the actual definition is in another file.
-#include "nearby-file.h"
+// editor
+#include "host-and-resource-name.h"    // HostAndResourceName
+
+// smbase
+#include "sm-macros.h"                 // DMEMB
+
+
+// An optional host and file name, and if the name is present, an
+// optional line number.
+class HostFileAndLineOpt {
+public:      // data
+  // Host and file name.  Can be empty() to mean none.
+  HostAndResourceName m_harn;
+
+  // The line number, or 0 to mean "none".
+  int m_line;
+
+public:      // funcs
+  HostFileAndLineOpt()
+    : m_harn(),
+      m_line(0)
+  {}
+
+  ~HostFileAndLineOpt()
+  {
+    // I'm a little worried about using this type as part of a Qt queued
+    // connection signal, so I want to ensure that a destroyed object is
+    // easily recognizable in the debugger.
+    m_line = -1000;
+  }
+
+  HostFileAndLineOpt(HostFileAndLineOpt const &obj)
+    : DMEMB(m_harn),
+      DMEMB(m_line)
+  {}
+
+  HostFileAndLineOpt(HostAndResourceName const &harn, int line)
+    : m_harn(harn),
+      m_line(line)
+  {}
+
+  bool hasFilename() const { return !m_harn.empty(); }
+  bool hasLine() const { return m_line != 0; }
+};
+
 
 #endif // EDITOR_HOST_FILE_AND_LINE_OPT_H
--- nearby-file.h
+++ nearby-file.h
@@ -6,54 +6,13 @@
 
 // editor
 #include "host-and-resource-name.h"    // HostAndResourceName
+#include "host-file-and-line-opt.h"    // HostFileAndLineOpt
 
 // smbase
 #include "array.h"                     // ArrayStack
-#include "sm-macros.h"                 // DMEMB
 #include "str.h"                       // string
 
 class SMFileUtil;                      // sm-file-util.h
-
-
-// An optional host and file name, and if the name is present, an
-// optional line number.
-//
-// TODO: Move this to its own module.
-class HostFileAndLineOpt {
-public:      // data
-  // Host and file name.  Can be empty() to mean none.
-  HostAndResourceName m_harn;
-
-  // The line number, or 0 to mean "none".
-  int m_line;
-
-public:      // funcs
-  HostFileAndLineOpt()
-    : m_harn(),
-      m_line(0)
-  {}
-
-  ~HostFileAndLineOpt()
-  {
-    // I'm a little worried about using this type as part of a Qt queued
-    // connection signal, so I want to ensure that a destroyed object is
-    // easily recognizable in the debugger.
-    m_line = -1000;
-  }
-
-  HostFileAndLineOpt(HostFileAndLineOpt const &obj)
-    : DMEMB(m_harn),
-      DMEMB(m_line)
-  {}
-
-  HostFileAndLineOpt(HostAndResourceName const &harn, int line)
-    : m_harn(harn),
-      m_line(line)
-  {}
-
-  bool hasFilename() const { return !m_harn.empty(); }
-  bool hasLine() const { return m_line != 0; }
-};
 
 
 // Interface with which to test the existence of a host+file.
